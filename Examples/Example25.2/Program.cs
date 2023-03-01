91c123c0218b229c03f2c8708835012188d084aa
Niuta <Niuta_87@mail.ru>
2023-03-01 20:53:49 +0600

    exponentiation2

---
+++ Examples/Example25.2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int number (int A, int B)
+{
+     int result = 1;
+     int i = 1;
+     while (i<=B)
+     {
+         result = result*A;
+         i++;
+     }     
+     return result;  
+}
+ Console.WriteLine($"{numberA} в степени {numberB} = {number(numberA,numberB)}"); 
