b2ec720a0214e2f1eddf43aa6820d362e7b06198
Niuta <Niuta_87@mail.ru>
2023-03-01 21:13:01 +0600

    sum of digits

---
+++ Examples/Example27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход 
+            число и выдаёт сумму цифр в числе.
+
+                452 -> 11
+
+                82 -> 10
+
+                9012 -> 12    */
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int number (int A)
+{
+     int sum = 0;
+     while (A != 0)
+     {
+         sum = sum + A%10;
+         A=A/10;
+     }     
+     return sum;  
+}
+ Console.WriteLine($"Сумма цифр числа {numberA} = {number(numberA)}");
