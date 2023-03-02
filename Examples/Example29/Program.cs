e03eb9a16160fa65fb95109d32d7cdbd8c37ed72
Niuta <Niuta_87@mail.ru>
2023-03-02 19:18:26 +0600

    array

---
+++ Examples/Example29/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+Console.Write("Введите число элементов массива: ");
+int array = Convert.ToInt32(Console.ReadLine());
+
+int[] GetBinaryArrey(int size)
+{
+   int[] array = new int[size];
+   for( int i = 0; i<size; i++)
+      {
+              array[i]=new Random().Next(1000);  
+      }  
+      return array; 
+}
+int[] resultArray = GetBinaryArrey(array);
+ Console.WriteLine($"Массив: [{String.Join(", ", resultArray)}]");
